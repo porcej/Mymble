d52411cf15b9d3edecaf2d6f92a636e472b0bf44
Joe Porcelli <porcej@gmail.com>
2023-05-02 12:41:45 -0400

    Added Pinia for state management

--- src/main.js
+++ src/main.js
@@ -1,5 +1,22 @@
+/**
+ * Admin Console
+ * @Mymble
+ * @author joe@kt3i.com
+ * @version 0.0.1
+ * @license MIT
+ */
+
 import { createApp } from 'vue'
+import { createPinia } from 'pinia';
+
 import './style.css'
 import App from './App.vue'
+
+const app = createApp(App);
+
+app.use(createPinia());
+// app.use(router);
+
+app.mount('#app');
 
 createApp(App).mount('#app')
