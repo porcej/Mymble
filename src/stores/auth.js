d3e2077dc036b308246d4b7612f3fe46ecf36f61
Joe Porcelli <porcej@gmail.com>
2023-06-30 14:25:28 -0400

    Added better commenting to auth store

--- src/stores/auth.js
+++ src/stores/auth.js
@@ -13,6 +13,13 @@ import { router} from '@/router';
 
 const baseUrl = `${import.meta.env.VITE_API_URL}`;
 
+
+/**
+ * Unpacks a JWT token returning its payload
+ *
+ * @params {String} token - token to parse
+ * @returns {Object} Parsed JWT payload
+ */
 function parseJwt (token) {
     var base64Url = token.split('.')[1];
     var base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
