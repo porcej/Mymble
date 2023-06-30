df0aab90d8afeb130ee9159776e259b183dfd77a
Joe Porcelli <porcej@gmail.com>
2023-06-30 08:23:43 -0400

    Updated Navigation bar to react to git status

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,10 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router';
+import { reactive, computed } from "vue";
 
 import { useAuthStore } from '@/stores';
 
 const authStore = useAuthStore();
-
-const pagePermitted = {
-  access: checkRoles("access"),
-  settings: checkRoles("settings"),
-  messenger: checkRoles("messenger"),
-};
 
 /**
  * Checks if roles are permitted - wraps fetch to handle an api with authentication 
@@ -17,17 +12,49 @@ const pagePermitted = {
  * @params {String} page page requesting access for
  * @returns {Bool} true iff roles are contained in those allowed by page
  */
-function checkRoles(page) {
-  const authorizedRoles = {
-    access: ["admin"],
-    settings: ["admin"],
-    messenger: ["messenger", "admin"],
-  };
-  if (!authStore.user || !authStore.user.roles) {
-    return false
-  } 
-  return authorizedRoles[page].some(rdx => authStore.user.roles.includes(rdx));
-}
+
+const authorizedRoles = {
+  access: ["admin"],
+  settings: ["admin"],
+  messenger: ["author"],
+  approver: ["approver"],
+};
+
+/**
+ * Computed method to determine if a user is authorized to view Settings Page
+ *
+ * @returns {Bool} true iff roles are contained in those allowed by page
+ */
+const showSettings = computed(() => {
+  if (authStore.user && authStore.user.roles) {
+    return authorizedRoles["settings"].some(rdx => authStore.user.roles.includes(rdx));
+  }
+  return false;
+});
+
+/**
+ * Computed method to determine if a user is authorized to view Messenger Page
+ *
+ * @returns {Bool} true iff roles are contained in those allowed by page
+ */
+const showMessenger = computed(() => {
+  if (authStore.user && authStore.user.roles) {
+    return authorizedRoles["messenger"].some(rdx => authStore.user.roles.includes(rdx));
+  }
+  return false;
+});
+
+/**
+ * Computed method to determine if a user is authorized to view Approver Page
+ *
+ * @returns {Bool} true iff roles are contained in those allowed by page
+ */
+const showApprover = computed(() => {
+  if (authStore.user && authStore.user.roles) {
+    return authorizedRoles["approver"].some(rdx => authStore.user.roles.includes(rdx));
+  }
+  return false;
+});
 
 </script>
 
@@ -38,11 +65,14 @@ function checkRoles(page) {
             <li class="nav-item">
               <RouterLink to="/" class="nav-item nav-link">Home</RouterLink>
             </li>
-            <li v-show="pagePermitted.settings" class="nav-item">
+            <li v-show="showSettings" class="nav-item">
               <RouterLink to="/settings" class="nav-item nav-link">Settings</RouterLink>
             </li>
-            <li v-show="pagePermitted.messenger" class="nav-item">
+            <li v-show="showMessenger" class="nav-item">
               <RouterLink to="/messenger" class="nav-item nav-link">Messenger</RouterLink>
+            </li>
+            <li v-show="showApprover" class="nav-item">
+              <RouterLink to="/approver" class="nav-item nav-link">Messege Approval</RouterLink>
             </li>
           </ul>
           <a @click="authStore.logout()" class="nav-item nav-link">Logout <i class="bi bi-box-arrow-right"></i></a>
